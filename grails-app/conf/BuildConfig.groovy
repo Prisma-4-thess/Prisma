7ea7777a60649ca8a6d85afd17054c5554795081
spiros <spirosbond@hotmail.com>
2014-02-27 19:52:22 +0200

    removed target folder

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -30,6 +30,7 @@ grails.project.dependency.resolution = {
 
         mavenLocal()
         mavenCentral()
+		mavenRepo 'http://repo.spring.io/milestone'
 
         // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
         //mavenRepo "http://snapshots.repository.codehaus.org"
