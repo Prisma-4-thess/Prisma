fb4907d228e323f351ee56b26536c0f053e37852
spirosbond <spirosbond@hotmail.com>
2014-03-03 02:09:48 +0200

    added specific and general search

--- grails-app/controllers/prisma/SearchController.groovy
+++ grails-app/controllers/prisma/SearchController.groovy
@@ -1,4 +1,5 @@
 package prisma
+import org.grails.datastore.gorm.finders.MethodExpression.IsNull;
 import org.grails.datastore.gorm.finders.MethodExpression.Like;
 
 import grails.plugin.springsecurity.annotation.Secured
@@ -28,30 +29,65 @@ class SearchController {
 		println params.tag
 		println params.fromDate
 		println params.toDate
-		println params.fromDate
 
 		def decision = new Decision()
 		def c = Decision.createCriteria()
-		if(!params.tag.empty){
-			decision = c.list {
-				like("subject","%"+params.subject+"%") &&
-						type{
-							like("label","%"+params.type+"%")
-						} &&
-						tags{
-							like("label","%"+params.tag+"%")
-						} &&
-						le("date",params.toDate) &&
-						ge("date",params.fromDate)
+
+		decision = c.list {
+
+			like("subject","%"+params.subject+"%")
+			type{
+				like("label","%"+params.type+"%")
 			}
-		}else{
-			decision = c.list {
-				like("subject","%"+params.subject+"%") &&
-						type{
-							like("label","%"+params.type+"%")
-						} &&
-						le("date",params.toDate) &&
-						ge("date",params.fromDate)
+			if(!params.tag.empty){
+				tags{
+					like("label","%"+params.tag+"%")
+				}
+			}
+			if(params.toDate!=null){
+				le("date",params.toDate)
+			}
+			if(params.fromDate!=null){
+				ge("date",params.fromDate)
+			}
+		}
+		[results:decision]
+	}
+
+	def searchspecific(){
+
+		println params.prot_num
+		println params.unit
+		println params.org
+		println params.signer_first
+		println params.signer_last
+		println params.fromDate
+		println params.toDate
+
+		def decision = new Decision()
+		def c = Decision.createCriteria()
+
+		decision = c.list {
+			if(!params.prot_num.empty){
+				like("protocolNumber","%"+params.prot_num+"%")
+			}
+			unit{
+				like("label","%"+params.unit+"%")
+			}
+			unit{
+				organization{
+					like("label","%"+params.org+"%")
+				}
+			}
+			signer{
+				if(!params.signer_first.empty) like("firstName","%"+params.signer_first+"%")
+				if(!params.signer_last.empty) like("lastName","%"+params.signer_last+"%")
+			}
+			if(params.toDate!=null){
+				le("date",params.toDate)
+			}
+			if(params.fromDate!=null){
+				ge("date",params.fromDate)
 			}
 		}
 		[results:decision]
--- grails-app/views/search/specific.gsp
+++ grails-app/views/search/specific.gsp
@@ -1,8 +1,38 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
 <html>
 <body>
-  <div class="body">
-  <p>specific search</p>
-  </div>
+ <div class="body">
+		<p>Specific search</p>
+		<g:formRemote name="specific"
+			url="[controller:'search', action:'searchspecific']" update="results">
+			<label for="prot_num">Protocol Number:</label>
+			<g:textField name="prot_num" />
+			<br />
+			<label for="unit">Unit:</label>
+			<g:textField name="unit" />
+			<br />
+			<label for="org">Organization:</label>
+			<g:textField name="org" />
+			<br />
+			<label for="signer_first">Signer first name:</label>
+			<g:textField name="signer_first" />
+			<label for="signer_last">Signer last name:</label>
+			<g:textField name="signer_last" />
+			<br />
+			<label for="fromDate">From Date:</label>
+			<g:datePicker name="fromDate" precision="day"
+				noSelection="['':'-No Selection-']" />
+			<br />
+			<label for="toDate">To Date:</label>
+			<g:datePicker name="toDate" precision="day"
+				noSelection="['':'-No Selection-']" />
+			<br />
+			<g:submitButton name="search" />
+		</g:formRemote>
+		<div id="results"
+			style='float: right; width: 80%; min-height: 500px; background-color: #c0ffc0;'>
+			<p>apotelesmata</p>
+		</div>
+	</div>
 </body>
 </html>
