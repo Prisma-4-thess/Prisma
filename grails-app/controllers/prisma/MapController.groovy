106237689173f2392d8f4335142f39ef1a5cab12
plutoattack <plutoattackme@gmail.com>
2014-03-16 13:49:54 +0200

    geo markers for multiple decision created

--- grails-app/controllers/prisma/MapController.groovy
+++ grails-app/controllers/prisma/MapController.groovy
@@ -41,21 +41,19 @@ class MapController {
 			lat=dec.geo.latitude
 			lon=dec.geo.longitude
 			dr=false
-			ds='<a href="'+grailsApplication.config.grails.serverURL+'/search/show/'+dec.id+'/?source=map'+'" target="_blank">'+dec.ada+'</a>'
+			ds='<a href="'+grailsApplication.config.grails.serverURL+'/map/show_geo/'+dec.geo.id+'/?source=map'+'" target="_blank">'+dec.geo+'</a>'
 			marker[i] = [latitude:lat, longitude:lon, draggable:dr,description:ds]
 			i++
 		}
-
-		/*lat=40.636
-		 lon=22.938
-		 dr=false
-		 marker[0] = [latitude:lat, longitude:lon, draggable:dr,description:ds]
-		 lat=40.65
-		 lon=22.92
-		 dr=false
-		 ds='πλατεία τζομπανογλου'
-		 marker[1] = [latitude:lat, longitude:lon, draggable:dr,description:ds]
-		[mark:marker]*/
 		render(template:"/common/map", model:[mark:marker])
+	}
+	def show_geo(){
+		def decisions=new Decision()
+		decisions=Decision.createCriteria().list{
+			geo{
+				eq("id",params.id.toLong())
+			}
+		}
+		render(template:"/common/decision_list", model:[results:decisions,decisionInstanceTotal:decisions.size(),source:"map"])
 	}
 }
