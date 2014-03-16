5fd0df5d19c5aed5feb87ac098958d6aa03d9452
spirosbond <spirosbond@hotmail.com>
2014-03-16 23:47:12 +0200

    show one marker for every position (mylo des to ligo)

--- grails-app/controllers/prisma/MapController.groovy
+++ grails-app/controllers/prisma/MapController.groovy
@@ -6,21 +6,26 @@ class MapController {
 	def decisions = new Decision()
 	def offset = "0"
 	def static maxToShow = 10
-	
+
 	def index() {
 		def decisionG=new Decision()
 		decisionG=Decision.createCriteria().list{ isNotNull("geo") }
 		println 'size'+decisionG.size()
 		def lat,lon,dr,ds
 		def marker=new Map[decisionG.size()]
+		def tempMarker = new Map[1]
 		int i=0
 		decisionG.each{ dec ->
 			lat=dec.geo.latitude
 			lon=dec.geo.longitude
 			dr=false
-			ds='<a href="'+grailsApplication.config.grails.serverURL+'Prisma/search/show/'+dec.id+'/?source=map'+'" target="_blank">'+dec.ada+'</a>'
-			marker[i] = [latitude:lat, longitude:lon, draggable:dr,description:ds]
-			i++
+			ds='<a href="'+grailsApplication.config.grails.serverURL+'/map/show_geo/'+dec.geo.id+'/?source=map'+'" target="_blank">'+dec.geo+'</a>'
+			tempMarker[0] = [latitude:lat, longitude:lon, draggable:dr,description:ds]
+			if(!marker.contains(tempMarker[0])){
+				println "new marker"
+				marker[i] = [latitude:lat, longitude:lon, draggable:dr,description:ds]
+				i++
+			}
 		}
 
 		/*lat=40.636
@@ -34,21 +39,26 @@ class MapController {
 		 marker[1] = [latitude:lat, longitude:lon, draggable:dr,description:ds]*/
 		[mark:marker]
 	}
-	
+
 	def homepage() {
 		def decisionG=new Decision()
 		decisionG=Decision.createCriteria().list{ isNotNull("geo") }
 		println 'size: '+decisionG.size()
 		def lat,lon,dr,ds
 		def marker=new Map[decisionG.size()]
+		def tempMarker = new Map[1]
 		int i=0
 		decisionG.each{ dec ->
 			lat=dec.geo.latitude
 			lon=dec.geo.longitude
 			dr=false
 			ds='<a href="'+grailsApplication.config.grails.serverURL+'/map/show_geo/'+dec.geo.id+'/?source=map'+'" target="_blank">'+dec.geo+'</a>'
-			marker[i] = [latitude:lat, longitude:lon, draggable:dr,description:ds]
-			i++
+			tempMarker[0] = [latitude:lat, longitude:lon, draggable:dr,description:ds]
+			if(!marker.contains(tempMarker[0])){
+				println "new marker"
+				marker[i] = [latitude:lat, longitude:lon, draggable:dr,description:ds]
+				i++
+			}
 		}
 		render(template:"/common/map", model:[mark:marker])
 	}
@@ -63,14 +73,14 @@ class MapController {
 		def toShow = Math.min(maxToShow, decisions.size())
 		render(view:"/map/markerList", model:[results:decisions.subList(0, toShow),decisionInstanceTotal:decisions.size(),source:"map"])
 	}
-	
+
 	def list() {
-		
-				offset=params.offset
-				def toShow = Math.min(Math.abs(decisions.size() - params.offset.toInteger()),maxToShow)
-				println "toShow: "+toShow
-				println "remaining: "+ (decisions.size())
-				println "source: "+params.source
-				render (template:"/common/decision_list", model:[results:decisions.subList(params.offset.toInteger(),params.offset.toInteger() + toShow), decisionInstanceTotal:(decisions.size()), source:params.source])
-			}
+
+		offset=params.offset
+		def toShow = Math.min(Math.abs(decisions.size() - params.offset.toInteger()),maxToShow)
+		println "toShow: "+toShow
+		println "remaining: "+ (decisions.size())
+		println "source: "+params.source
+		render (template:"/common/decision_list", model:[results:decisions.subList(params.offset.toInteger(),params.offset.toInteger() + toShow), decisionInstanceTotal:(decisions.size()), source:params.source])
+	}
 }
