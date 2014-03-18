fa4314d3b58a41c6ba532a827aac5244c84bb16a
plutoattack <plutoattackme@gmail.com>
2014-03-18 17:46:47 +0200

    add geo web

--- grails-app/controllers/prisma/DefineGeoController.groovy
+++ grails-app/controllers/prisma/DefineGeoController.groovy
@@ -1,7 +1,20 @@
 package prisma
 import grails.plugin.springsecurity.annotation.Secured
-@Secured(['ROLE_ADMIN'])
+@Secured(['ROLE_ADMIN','ROLE_USER'])
 class DefineGeoController {
 
-    def index() { }
+    def index() { 
+	}
+	def submitGeo(){
+		def udg=new Userdefgeo()
+		udg.latitude=params.lat
+		udg.longitude=params.lng
+		udg.address=params.address
+		udg.namegrk=params.namegrk
+		udg.save(flush:true)
+		def udd=new Userdefdec()
+		udd.decision=Decision.get(params.decisionId.toLong())
+		udd.geo=udg
+		udd.save(flush:true)
+	}
 }
