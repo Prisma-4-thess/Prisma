6c900f97087ceea28bdcbee2f8b1f7ea82646c60
spirosbond <spirosbond@hotmail.com>
2014-10-04 22:26:56 +0200

    I have no idea what I am pushing now...

---
+++ test/unit/prisma/ContextualSearchControllerTests.groovy
@@ -0,0 +1,16 @@
+package prisma
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(ContextualSearchController)
+class ContextualSearchControllerTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
